38dc4f104f46d7126bb2a11498c76ac85f277fd4
JayJatoba <jguilhermejatoba@hotmail.com>
2020-06-15 10:01:29 -0300

    Criação Projeto
    Criação do projeto e do pacote de enum.

---
+++ Projeto_P1/proj_P1/src/br/maua/Enums/EstadoPedido.java
@@ -0,0 +1,5 @@
+package br.maua.Enums;
+
+public enum EstadoPedido {
+    REALIZADO,PREPARACAO,SAIU_PARA_ENTREGA,ENTREGUE,DEVOLVIDO;
+}
---
+++ Projeto_P1/proj_P1/src/br/maua/Enums/TipoPagamento.java
@@ -0,0 +1,5 @@
+package br.maua.Enums;
+
+public enum TipoPagamento {
+    DINHEIRO, DEBITO,CREDITO,VALE_ALIMENTACAO,VALE_REFEICAO;
+}
