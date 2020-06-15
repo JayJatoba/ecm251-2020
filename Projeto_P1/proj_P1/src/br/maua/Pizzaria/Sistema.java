49761750ca1775a9255965e7c8c405db94bc29c3
JayJatoba <jguilhermejatoba@hotmail.com>
2020-06-15 19:24:20 -0300

    Update Sistema.java
    Variável valor não pode ser negativa ou igual à 0.

--- Projeto_P1/proj_P1/src/br/maua/Pizzaria/Sistema.java
+++ Projeto_P1/proj_P1/src/br/maua/Pizzaria/Sistema.java
@@ -11,12 +11,11 @@ import br.maua.models.Pedido;
 import br.maua.models.Usuario;
 
 public class Sistema {
-    private static final String senha = "123456";
 
     public static void run(){
         ArrayList<Pedido> listaPedidos = new ArrayList<>();
         boolean flag = true;
-        Usuario user = new Usuario("MZ", "mz@email.com", senha);
+        Usuario user = new Usuario("MZ", "mz@email.com", "123456");
         
         
         while (flag){
@@ -37,79 +36,7 @@ public class Sistema {
 
             switch (escolha) {
                 case 1:
-                if(user.autenticarSenha()){
-                    System.out.println("Qual a descrição do pedido?");
-                    String descricao = sc.nextLine();
-                    System.out.println("Qual valor do pedido?");
-                    float valor;
-                    String valorStr = sc.nextLine();
-                    while(true){
-                    try {
-                        valor = Float.parseFloat(valorStr);
-                        break;
-                 }
-                 // Erro para valor que não é número float,
-                 // que com default vai para 0
-                    catch (NumberFormatException e)
-                 {
-                        System.out.println("Digite valor em float.");
-                        valorStr = sc.nextLine();
-                 }}
-
-                
-                    System.out.println("Opções de pagamento:\n1 - Dinheiro");
-                    System.out.println("2 - Débito\n3 - Crédito\n4 - Vale Alimentação");
-                    System.out.println("5 - Vale Refeição");
-                    
-                    boolean flag2 = true;
-                    int metodo=0;
-                    while(flag2){
-                        opcao = sc.nextLine();
-                        
-                        try {
-                            metodo = Integer.parseInt(opcao);
-}
-                        catch (NumberFormatException e){
-                            metodo = 0;}
-                        switch (metodo) {
-                            case 1:
-                                listaPedidos.add(new Pedido(descricao, valor, TipoPagamento.DINHEIRO));
-                                System.out.println("Pedido realizado.");
-                                flag2 = false;
-                                break;
-                            case 2:
-                                listaPedidos.add(new Pedido(descricao, valor, TipoPagamento.DEBITO));
-                                System.out.println("Pedido realizado.");
-                                flag2 = false;
-                                break;
-                            case 3:
-                                listaPedidos.add(new Pedido(descricao, valor, TipoPagamento.CREDITO));
-                                System.out.println("Pedido realizado.");
-                                flag2 = false;
-                                break;
-                            case 4:
-                                listaPedidos.add(new Pedido(descricao, valor, TipoPagamento.VALE_ALIMENTACAO));
-                                System.out.println("Pedido realizado.");
-                                flag2 = false;
-                                break;
-                            case 5:
-                                listaPedidos.add(new Pedido(descricao, valor, TipoPagamento.VALE_REFEICAO));
-                                System.out.println("Pedido realizado.");
-                                flag2 = false;
-                                break;
-                            
-                            default:
-                                System.out.println("Valores aceitos somente de 1 a 5.");
-                                break;}
-                    }
-                    
-                        
-                }//Final do if de senha aceita
-                else{
-                    System.out.println("Autorização negada.");
-                }
-                    
-
+                    fazerPedido(listaPedidos, user);
                     break;
 
                 case 2:
@@ -117,65 +44,7 @@ public class Sistema {
                     break;
 
                 case 3:
-
-                    if(verificaLista(listaPedidos)){
-                        if (user.autenticarSenha()){
-                            mostrarPedidos(listaPedidos);
-                            System.out.println("Qual o id do pedido a ser alterado?");
-                            String idPedido = sc.nextLine();
-                            boolean alteracao = false;
-                            for (Pedido pedido : listaPedidos) {
-                                if(pedido.getId().equals(idPedido)){
-                                    alteracao = true;
-                                    System.out.println("Estado para ser mudado:\n1 - PREPARACAO");
-                                    System.out.println("2 - SAIU_PARA_ENTREGA\n3 - ENTREGUE\n4 - DEVOLVIDO");
-                                
-                                    boolean flag2 = true;
-                                    int metodo=0;
-                                    while(flag2){
-                                        opcao = sc.nextLine();
-                                    
-                                        try {
-                                            metodo = Integer.parseInt(opcao);}
-                                        catch (NumberFormatException e){
-                                            metodo = 0;}
-                                    switch (metodo) {
-                                        case 1:
-                                            pedido.setEstadoPedido(EstadoPedido.PREPARACAO);
-                                            flag2 = false;
-                                            break;
-                                        case 2:
-                                            pedido.setEstadoPedido(EstadoPedido.SAIU_PARA_ENTREGA);
-                                            flag2 = false;
-                                            break;
-                                        case 3:
-                                            pedido.setEstadoPedido(EstadoPedido.ENTREGUE);
-                                            flag2 = false;
-                                            break;
-                                        case 4:
-                                            pedido.setEstadoPedido(EstadoPedido.DEVOLVIDO);
-                                            flag2 = false;
-                                            break;
-                                
-                                        default:
-                                            System.out.println("Valores aceitos somente de 1 a 4.");
-                                            break;}
-                                        System.out.println("Estado do pedido alterado.");
-                                break;}}//Termina o foreach
-                            
-                        }
-                            if (!alteracao){
-                                System.out.println("Pedido com id = "+idPedido+" não encontrado.");
-                            }    
-                    }// Final do if de senha aceita
-                        else{
-                            System.out.println("Autorização negada para trocar estado de pedido.");
-                        } 
-                    }   // Lista não está vazia
-                    else{
-                        System.out.println("Lista está vazia.");
-                    }
-                    
+                    alterarEstado(listaPedidos, user);
                     break;
 
                 case 0:
@@ -195,18 +64,155 @@ public class Sistema {
 
 
 
+    public static void fazerPedido(ArrayList<Pedido> listaPedidos, Usuario user){
+        if(user.autenticarSenha()){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Qual a descrição do pedido?");
+            String descricao = sc.nextLine();
+            System.out.println("Qual valor do pedido?");
+            float valor = 0;
+            String valorStr = sc.nextLine();
+            while(true){
+            try {
+                valor = Float.parseFloat(valorStr);
+                if(valor<=0){
+                    throw new IllegalArgumentException("Valor da compra não pode ser negativo.");
+                }
+                break;
+                
+         }
+         // Erro para valor que não é número float,
+         // que com default vai para 0
+            catch (NumberFormatException e)
+         {
+                System.out.println("Digite valor em float. (Para separar as casas decimais, usar '.')");
+                valorStr = sc.nextLine();
+         }
+        //  Erro para valor menor ou igual a 0. Isto não seria uma compra aceita.
+            catch(IllegalArgumentException e){
+                System.out.println(e.getMessage());
+                System.out.println("Qual o valor da compra? (em float e positivo)");
+                valorStr = sc.nextLine();
+            }}
 
-    // public void alterarEstado(){
-    //     // Para alterar estados de pedidos,
-    //     // deve-se ver se há pedidos.
+        
+            System.out.println("Opções de pagamento:\n1 - Dinheiro");
+            System.out.println("2 - Débito\n3 - Crédito\n4 - Vale Alimentação");
+            System.out.println("5 - Vale Refeição");
+            
+            boolean flag2 = true;
+            int metodo=0;
+            while(flag2){
+                String opcao = sc.nextLine();
+                
+                try {
+                    metodo = Integer.parseInt(opcao);
+}
+                catch (NumberFormatException e){
+                    metodo = 0;}
+                switch (metodo) {
+                    case 1:
+                        listaPedidos.add(new Pedido(descricao, valor, TipoPagamento.DINHEIRO));
+                        System.out.println("Pedido realizado.");
+                        flag2 = false;
+                        break;
+                    case 2:
+                        listaPedidos.add(new Pedido(descricao, valor, TipoPagamento.DEBITO));
+                        System.out.println("Pedido realizado.");
+                        flag2 = false;
+                        break;
+                    case 3:
+                        listaPedidos.add(new Pedido(descricao, valor, TipoPagamento.CREDITO));
+                        System.out.println("Pedido realizado.");
+                        flag2 = false;
+                        break;
+                    case 4:
+                        listaPedidos.add(new Pedido(descricao, valor, TipoPagamento.VALE_ALIMENTACAO));
+                        System.out.println("Pedido realizado.");
+                        flag2 = false;
+                        break;
+                    case 5:
+                        listaPedidos.add(new Pedido(descricao, valor, TipoPagamento.VALE_REFEICAO));
+                        System.out.println("Pedido realizado.");
+                        flag2 = false;
+                        break;
+                    
+                    default:
+                        System.out.println("Valores aceitos somente de 1 a 5.");
+                        break;}
+            }
+            
+                
+        }//Final do if de senha aceita
+        else{
+            System.out.println("Autorização negada.");
+        }
+    }
 
-    //     if (verificaLista()){
 
-    //     }
-    //     else{
-    //         System.out.println("Não há pedidos para mudar.");
-    //     }
-    // }
+
+
+
+    public static void alterarEstado(ArrayList<Pedido> listaPedidos, Usuario user){
+        if(verificaLista(listaPedidos)){
+            if (user.autenticarSenha()){
+                Scanner sc = new Scanner(System.in);
+                mostrarPedidos(listaPedidos);
+                System.out.println("Qual o id do pedido a ser alterado?");
+                String idPedido = sc.nextLine();
+                boolean alteracao = false;
+                for (Pedido pedido : listaPedidos) {
+                    if(pedido.getId().equals(idPedido)){
+                        alteracao = true;
+                        System.out.println("Estado para ser mudado:\n1 - PREPARACAO");
+                        System.out.println("2 - SAIU_PARA_ENTREGA\n3 - ENTREGUE\n4 - DEVOLVIDO");
+                    
+                        boolean flag2 = true;
+                        int metodo=0;
+                        while(flag2){
+                            String opcao = sc.nextLine();
+                        
+                            try {
+                                metodo = Integer.parseInt(opcao);}
+                            catch (NumberFormatException e){
+                                metodo = 0;}
+                        switch (metodo) {
+                            case 1:
+                                pedido.setEstadoPedido(EstadoPedido.PREPARACAO);
+                                flag2 = false;
+                                break;
+                            case 2:
+                                pedido.setEstadoPedido(EstadoPedido.SAIU_PARA_ENTREGA);
+                                flag2 = false;
+                                break;
+                            case 3:
+                                pedido.setEstadoPedido(EstadoPedido.ENTREGUE);
+                                flag2 = false;
+                                break;
+                            case 4:
+                                pedido.setEstadoPedido(EstadoPedido.DEVOLVIDO);
+                                flag2 = false;
+                                break;
+                    
+                            default:
+                                System.out.println("Valores aceitos somente de 1 a 4.");
+                                break;}
+                            System.out.println("Estado do pedido alterado.");
+                    break;}}//Termina o foreach
+                
+            }
+                if (!alteracao){
+                    System.out.println("Pedido com id = "+idPedido+" não encontrado.");
+                }    
+        }// Final do if de senha aceita
+            else{
+                System.out.println("Autorização negada para trocar estado de pedido.");
+            } 
+        }   // Lista não está vazia
+        else{
+            System.out.println("Lista está vazia.");
+        }
+    }
         
 
     public static boolean verificaLista(ArrayList<Pedido> listaPedidos){
@@ -223,7 +229,7 @@ public class Sistema {
             }
         }
         else{
-            System.out.println("Lista de pedidos vazia.");
+            System.out.println("Lista de pedidos está vazia.");
         }
         
     }
