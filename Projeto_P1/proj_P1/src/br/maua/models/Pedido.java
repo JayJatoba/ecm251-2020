194bcd01a0a87cafeeff1a94c79bdb11a0e58a97
JayJatoba <jguilhermejatoba@hotmail.com>
2020-06-22 11:15:06 -0300

    Update Pedido.java
    Javadoc completo da classe Pedido

--- Projeto_P1/proj_P1/src/br/maua/models/Pedido.java
+++ Projeto_P1/proj_P1/src/br/maua/models/Pedido.java
@@ -25,6 +25,12 @@ public class Pedido {
     private EstadoPedido estadoPedido;
 
 
+    
+    /** 
+     * Método que cria uma String dedicada a cada pedido
+     * 
+     * @return String
+     */
     private String geradorId(){
         Random random = new Random();
         String idGerado = "";
@@ -34,36 +40,92 @@ public class Pedido {
         return idGerado;
     }
 
+    
+    /** 
+     * Método de descrição de cada pedido
+     * 
+     * 
+     * @return String com todos os dados
+     */
     @Override
     public String toString() {
         return "Pedido [descricao=" + descricao + ", estadoPedido=" + estadoPedido + ", id=" + id + ", pagamento="
                 + pagamento + ", valor=" + valor + "]";
     }
 
+    
+    /** 
+     * Método de retorno somente da descrição
+     * 
+     * @return String 
+     */
     public String getDescricao() {
         return descricao;
     }
 
+    
+    /** 
+     * Retorna o tipo de pagamento
+     * 
+     * @return TipoPagamento
+     */
     public TipoPagamento getPagamento() {
         return pagamento;
     }
 
+    
+    /** 
+     * Retorna o ID do pagamento.
+     * 
+     * @return String
+     */
     public String getId() {
         return id;
     }
 
+    
+    /** 
+     * Método que retorna o valor do pedido
+     * 
+     * @return float
+     */
     public float getValor() {
         return valor;
     }
 
+    
+    /** 
+     * 
+     * Método que retorna o estado atual do pedido
+     * 
+     * @return EstadoPedido
+     */
     public EstadoPedido getEstadoPedido() {
         return estadoPedido;
     }
 
+    
+    /** 
+     * Método que muda o estado do pedido
+     * Recebe o estado que será o novoestado do pedido
+     * 
+     * @param estadoPedido
+     */
     public void setEstadoPedido(EstadoPedido estadoPedido) {
         this.estadoPedido = estadoPedido;
     }
 
+
+    /**
+     * 
+     * Método construtor da classe.
+     * Iniciado quando um pedido novo é feito.
+     * Recebe descrição do pedido, valor e forma de pagamento.
+     * 
+     * @param descricao
+     * @param valor
+     * @param pagamento
+     */
     public Pedido(String descricao, float valor, TipoPagamento pagamento) {
         this.descricao = descricao;
         this.valor = valor;
