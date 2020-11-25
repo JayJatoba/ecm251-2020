5f99cf7eb741b4b5ec14727cebedaa1b55114156
André Lebrão <61150819+andledrao@users.noreply.github.com>
2020-11-25 10:47:44 -0300

    ItensParser

---
+++ Proj_T4/src/br/maua/parsers/ItensParser.java
@@ -0,0 +1,28 @@
+package br.maua.parsers;
+import br.maua.model.Item;
+import org.json.JSONObject;
+/**
+ * Responsavel pela transformacao do objeto json em um objeto do tipo Item vice e versa
+ * @author Andre Lebrao 18.00356-7
+ * @author Joao Guilherme Jatoba 18.01790-8
+ * @since 28/09/2020
+ */
+public class ItensParser {
+
+    public static JSONObject toJSON(Item item){
+        JSONObject json = new JSONObject();
+        json.put("nome",item.getNome());
+        json.put("preco",item.getPreco());
+        json.put("peso",item.getPeso());
+        json.put("quant",item.getQuant());
+        return json;
+    }
+    /**
+     * Transforma um objeto json em um objeto Item
+     * @param json Objeto json que tem os dados de um anime
+     * @return Objeto do tipo anime
+     */
+    public static Item fromJSON(JSONObject json){
+        return new Item(json.getString("nome"),json.getFloat("preco"),json.getFloat("peso"),json.getInt("quant"));
+    }
+}
